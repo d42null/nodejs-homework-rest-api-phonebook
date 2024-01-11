6c5b47ad0eebf6cb7bec6c50fd9467fd1fd44393
d42null <d2null@gmail.com>
2024-01-11 16:06:32 +0200

    " removed from required field message

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,7 +3,7 @@ const HttpError = require("../utils/HttpError");
 const validateBody = (schema) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body, { abortEarly: false });
-    if (error) next(HttpError(400, error.message));
+    if (error) next(HttpError(400, error.message.replace(/"/g, "")));
     next();
   };
 };
